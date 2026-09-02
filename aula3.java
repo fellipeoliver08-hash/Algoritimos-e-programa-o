7aef624b1f6fdbe788ff30da5fc0e97c964c83c6
fellipeoliver08-hash <fellipeoliver08@gmail.com>
2026-09-02 10:06:19 -0300

    Add files via upload

---
+++ aula3.java
@@ -0,0 +1,11 @@
+
+public class aula3 {
+    public static void main (String[] args){
+
+      
+       
+        System.out.println("Fellipe Cera de Oliveira");
+        System.out.println("1143482647");
+        System.out.println("Análise e Desenvolvimento de Sistemas");
+    }
+}
---
+++ exercicio2.java
@@ -0,0 +1,9 @@
+public class exercicio2 {
+    public static void main (String[] args){
+
+        System.out.println("Minha idade é " + 19 + " anos");
+        System.out.println("Eu tenho " + 1.76 + " de altura ");
+        System.out.println("Status na aula: Presente");
+    }
+
+}
